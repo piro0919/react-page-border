f613ffa51909e98ff4aab2d1fe8cbabcb57f67da
piro0919 <piro.haniwa@gmail.com>
2024-10-28 03:44:08 +0900

    feat: hoge

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,8 +2,8 @@ import styles from "./style.module.css";
 
 export default function Page(): JSX.Element {
   return (
-    <div className={styles.wrapper}>
-      <div className={styles.wrapper2}>
+    <div className={styles.borderWrapper}>
+      <div className={styles.borderInner}>
         <p>
           aaa
           <br />
@@ -220,6 +220,10 @@ export default function Page(): JSX.Element {
           aaa
         </p>
       </div>
+      <div className={`${styles.corner} ${styles.cornerTopLeft}`} />
+      <div className={`${styles.corner} ${styles.cornerTopRight}`} />
+      <div className={`${styles.corner} ${styles.cornerBottomRight}`} />
+      <div className={`${styles.corner} ${styles.cornerBottomLeft}`} />
     </div>
   );
 }
