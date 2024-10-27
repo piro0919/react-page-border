1869c232b89f1e005f8567fd4b189295c441213d
piro0919 <piro.haniwa@gmail.com>
2024-10-28 03:17:44 +0900

    feat: hoge

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,221 +3,223 @@ import styles from "./style.module.css";
 export default function Page(): JSX.Element {
   return (
     <div className={styles.wrapper}>
-      <p>
-        aaa
-        <br />
-        aaa
-        <br />
-        aaa
-        <br />
-        aaa
-        <br />
-        aaa
-        <br />
-        aaa
-        <br />
-        aaa
-        <br />
-        aaa
-        <br />
-        aaa
-        <br />
-        aaa
-        <br />
-        aaa
-        <br />
-        aaa
-        <br />
-        aaa
-        <br />
-        aaa
-        <br />
-        aaa
-        <br />
-        aaa
-        <br />
-        aaa
-        <br />
-        aaa
-        <br />
-        aaa
-        <br />
-        aaa
-        <br />
-        aaa
-        <br />
-        aaa
-        <br />
-        aaa
-        <br />
-        aaa
-        <br />
-        aaa
-        <br />
-        aaa
-        <br />
-        aaa
-        <br />
-        aaa
-        <br />
-        aaa
-        <br />
-        aaa
-        <br />
-        aaa
-        <br />
-        aaa
-        <br />
-        aaa
-        <br />
-        aaa
-        <br />
-        aaa
-        <br />
-        aaa
-        <br />
-        aaa
-        <br />
-        aaa
-        <br />
-        aaa
-        <br />
-        aaa
-        <br />
-        aaa
-        <br />
-        aaa
-        <br />
-        aaa
-        <br />
-        aaa
-        <br />
-        aaa
-        <br />
-        aaa
-        <br />
-        aaa
-        <br />
-        aaa
-        <br />
-        aaa
-        <br />
-        aaa
-        <br />
-        aaa
-        <br />
-        aaa
-        <br />
-        aaa
-        <br />
-        aaa
-        <br />
-        aaa
-        <br />
-        aaa
-        <br />
-        aaa
-        <br />
-        aaa
-        <br />
-        aaa
-        <br />
-        aaa
-        <br />
-        aaa
-        <br />
-        aaa
-        <br />
-        aaa
-        <br />
-        aaa
-        <br />
-        aaa
-        <br />
-        aaa
-        <br />
-        aaa
-        <br />
-        aaa
-        <br />
-        aaa
-        <br />
-        aaa
-        <br />
-        aaa
-        <br />
-        aaa
-        <br />
-        aaa
-        <br />
-        aaa
-        <br />
-        aaa
-        <br />
-        aaa
-        <br />
-        aaa
-        <br />
-        aaa
-        <br />
-        aaa
-        <br />
-        aaa
-        <br />
-        aaa
-        <br />
-        aaa
-        <br />
-        aaa
-        <br />
-        aaa
-        <br />
-        aaa
-        <br />
-        aaa
-        <br />
-        aaa
-        <br />
-        aaa
-        <br />
-        aaa
-        <br />
-        aaa
-        <br />
-        aaa
-        <br />
-        aaa
-        <br />
-        aaa
-        <br />
-        aaa
-        <br />
-        aaa
-        <br />
-        aaa
-        <br />
-        aaa
-        <br />
-        aaa
-        <br />
-        aaa
-        <br />
-        aaa
-        <br />
-        aaa
-        <br />
-        aaa
-        <br />
-        aaa
-        <br />
-        aaa
-        <br />
-        aaa
-        <br />
-        aaa
-        <br />
-        aaa
-      </p>
+      <div className={styles.wrapper2}>
+        <p>
+          aaa
+          <br />
+          aaa
+          <br />
+          aaa
+          <br />
+          aaa
+          <br />
+          aaa
+          <br />
+          aaa
+          <br />
+          aaa
+          <br />
+          aaa
+          <br />
+          aaa
+          <br />
+          aaa
+          <br />
+          aaa
+          <br />
+          aaa
+          <br />
+          aaa
+          <br />
+          aaa
+          <br />
+          aaa
+          <br />
+          aaa
+          <br />
+          aaa
+          <br />
+          aaa
+          <br />
+          aaa
+          <br />
+          aaa
+          <br />
+          aaa
+          <br />
+          aaa
+          <br />
+          aaa
+          <br />
+          aaa
+          <br />
+          aaa
+          <br />
+          aaa
+          <br />
+          aaa
+          <br />
+          aaa
+          <br />
+          aaa
+          <br />
+          aaa
+          <br />
+          aaa
+          <br />
+          aaa
+          <br />
+          aaa
+          <br />
+          aaa
+          <br />
+          aaa
+          <br />
+          aaa
+          <br />
+          aaa
+          <br />
+          aaa
+          <br />
+          aaa
+          <br />
+          aaa
+          <br />
+          aaa
+          <br />
+          aaa
+          <br />
+          aaa
+          <br />
+          aaa
+          <br />
+          aaa
+          <br />
+          aaa
+          <br />
+          aaa
+          <br />
+          aaa
+          <br />
+          aaa
+          <br />
+          aaa
+          <br />
+          aaa
+          <br />
+          aaa
+          <br />
+          aaa
+          <br />
+          aaa
+          <br />
+          aaa
+          <br />
+          aaa
+          <br />
+          aaa
+          <br />
+          aaa
+          <br />
+          aaa
+          <br />
+          aaa
+          <br />
+          aaa
+          <br />
+          aaa
+          <br />
+          aaa
+          <br />
+          aaa
+          <br />
+          aaa
+          <br />
+          aaa
+          <br />
+          aaa
+          <br />
+          aaa
+          <br />
+          aaa
+          <br />
+          aaa
+          <br />
+          aaa
+          <br />
+          aaa
+          <br />
+          aaa
+          <br />
+          aaa
+          <br />
+          aaa
+          <br />
+          aaa
+          <br />
+          aaa
+          <br />
+          aaa
+          <br />
+          aaa
+          <br />
+          aaa
+          <br />
+          aaa
+          <br />
+          aaa
+          <br />
+          aaa
+          <br />
+          aaa
+          <br />
+          aaa
+          <br />
+          aaa
+          <br />
+          aaa
+          <br />
+          aaa
+          <br />
+          aaa
+          <br />
+          aaa
+          <br />
+          aaa
+          <br />
+          aaa
+          <br />
+          aaa
+          <br />
+          aaa
+          <br />
+          aaa
+          <br />
+          aaa
+          <br />
+          aaa
+          <br />
+          aaa
+          <br />
+          aaa
+          <br />
+          aaa
+          <br />
+          aaa
+          <br />
+          aaa
+          <br />
+          aaa
+          <br />
+          aaa
+          <br />
+          aaa
+          <br />
+          aaa
+          <br />
+          aaa
+        </p>
+      </div>
     </div>
   );
 }
